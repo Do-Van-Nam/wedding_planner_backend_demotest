856f664ff4868caca120192af70f04623fca38b5
Nam Đỗ Văn <nam15sep2003@gmail.com>
2024-08-27 23:34:26 +0700

    27/8

--- src/app/controllers/RoomController.js
+++ src/app/controllers/RoomController.js
@@ -37,6 +37,36 @@ const createRoom = async (req, res) => {
         return res.status(400).json({ message: 'Server error' })
     }
 }
+const quickCreateRooms = async (req, res) => {
+    const { noFloor,noRoomPerFloor, price,buildingId} = req.body
+    try {
+        let rooms=[]
+        for (let i = 1; i <= noFloor; i++) { 
+            for (let j = 1; j <= noRoomPerFloor; j++) { 
+                let roomName= j < 10 ? `${i}0${j}` : `${i}${j}`
+                let roomExists = await Room.findOne({buildingId,roomName})
+                if(!roomExists){
+                    
+                    rooms.push({
+                    roomName, 
+                    tenantId: null,
+                    buildingId: buildingId,
+                    floor: i, 
+                    isRented:false,price:price,startday:null,noPrepaidMonths:0
+                })
+                }
+                else console.log(roomExists) 
+                
+                
+            }
+        }
+        await Room.insertMany(rooms)
+        return res.status(201).json({ message: 'Rooms created successfully!' });
+    } catch (error) {
+        console.log(error)
+        return res.status(400).json({ message: 'Server error' })
+    }
+}
 const updateRoom = async (req, res) => {
     const id = req.params.id
     let { roomName,tenantId,buildingId,floor,isRented,price,startday,noPrepaidMonths } = req.body
@@ -69,5 +99,33 @@ const deleteRoom = async (req, res) => {
     }
 
 }
+const deleteRoomOnOneFloor = async (req, res) => {
+    const {buildingId,floor} = req.params
+    try {
+        const deletedRoom = await Room.deleteMany({buildingId:buildingId,floor:floor})
+        if (deletedRoom.deletedCount===0) {
+            return res.status(404).json({ message: 'Room not found' })
+        }
+        return res.status(200).json({ message: 'Room deleted successfully!' })
 
-module.exports = { getRoomsByBuildingId, getRoomById, createRoom, updateRoom, deleteRoom }
+    } catch (error) {
+        res.status(400).json({ message: error.message });
+    }
+
+}
+const deleteRoomByBuildingId = async (req, res) => {
+    const {buildingId} = req.params
+    try {
+        const deletedRoom = await Room.deleteMany({buildingId})
+        if (deletedRoom.deletedCount===0) {
+            return res.status(404).json({ message: 'Room not found' })
+        }
+        return res.status(200).json({ message: 'Room deleted successfully!' })
+
+    } catch (error) {
+        res.status(400).json({ message: error.message });
+    }
+
+}
+
+module.exports = {deleteRoomByBuildingId,deleteRoomOnOneFloor, getRoomsByBuildingId, getRoomById, createRoom,quickCreateRooms, updateRoom, deleteRoom }
--- src/routes/room.js
+++ src/routes/room.js
@@ -1,12 +1,15 @@
 const express = require('express')
 const router = express.Router()
-const { getRoomsByBuildingId,getRoomById, updateRoom, deleteRoom, createRoom } = require('../app/controllers/RoomController')
+const {deleteRoomByBuildingId,deleteRoomOnOneFloor, getRoomsByBuildingId,getRoomById, updateRoom, deleteRoom, createRoom,quickCreateRooms } = require('../app/controllers/RoomController')
 const managerAuthMiddleware = require('../app/middlewares/managerAuthMiddleware')
 
 // router.get('/:id', getRoomById)
 router.get('/:buildingId',managerAuthMiddleware, getRoomsByBuildingId)
 router.put('/:id', updateRoom)
-router.delete('/:id', deleteRoom)
 router.post('/', createRoom)
+router.post('/quickCreate',managerAuthMiddleware, quickCreateRooms)
+router.delete('/:buildingId/:floor',managerAuthMiddleware, deleteRoomOnOneFloor)
+router.delete('/:buildingId',managerAuthMiddleware, deleteRoomByBuildingId)
+// router.delete('/:id', deleteRoom)
 
 module.exports = router
