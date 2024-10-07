a956ad5e999cb7a2b86d806137128c4a7998a18c
Do-Van-Nam <145846257+Do-Van-Nam@users.noreply.github.com>
2024-10-07 22:07:02 +0700

    Update room.js

--- src/routes/room.js
+++ src/routes/room.js
@@ -1,7 +1,7 @@
 const express = require('express')
 const router = express.Router()
 const {deleteRoomByBuildingId,deleteRoomOnOneFloor, getRoomsByBuildingId,getRoomById, updateRoom, deleteRoom, createRoom,quickCreateRooms } = require('../app/controllers/RoomController')
-const managerAuthMiddleware = require('../app/middlewares/managerAuthMiddleware')
+const {managerAuthMiddleware} = require('../app/middlewares/managerAuthMiddleware')
 
 // router.get('/:id', getRoomById)
 router.get('/:buildingId',managerAuthMiddleware, getRoomsByBuildingId)
