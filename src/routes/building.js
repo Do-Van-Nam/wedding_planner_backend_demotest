b393fe5cf6d9c854f628aa90ef7402dae853f9e8
Do-Van-Nam <145846257+Do-Van-Nam@users.noreply.github.com>
2024-10-07 22:06:42 +0700

    Update building.js

--- src/routes/building.js
+++ src/routes/building.js
@@ -1,7 +1,7 @@
 const express = require('express')
 const router = express.Router()
 const { getBuildingsByOwnerId,getBuildingById, updateBuilding, deleteBuilding, createBuilding } = require('../app/controllers/BuildingController')
-const managerAuthMiddleware = require('../app/middlewares/managerAuthMiddleware')
+const {managerAuthMiddleware} = require('../app/middlewares/managerAuthMiddleware')
 
 // router.get('/:id', getBuildingById)
 router.get('/:ownerId',managerAuthMiddleware, getBuildingsByOwnerId)
